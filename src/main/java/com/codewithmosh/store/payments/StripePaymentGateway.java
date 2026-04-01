71c95f03e9df1703c8c74a1212f9fcf7daf56548
hhajjem <habib.hajjem@talan.com>
2026-04-01 11:17:05 +0100

    fix setting metadata

--- src/main/java/com/codewithmosh/store/payments/StripePaymentGateway.java
+++ src/main/java/com/codewithmosh/store/payments/StripePaymentGateway.java
@@ -29,7 +29,7 @@ public class StripePaymentGateway implements PaymentGateway {
                     .setMode(SessionCreateParams.Mode.PAYMENT)
                     .setSuccessUrl(websiteUrl + "/checkout-success?orderId=" + order.getId())
                     .setCancelUrl(websiteUrl + "/checkout-cancel")
-                    .putMetadata("order_id", order.getId().toString());
+                    .setPaymentIntentData(createPaymentIntentData(order));
 
             order.getItems().forEach(item -> {
                 var lineItem = createLineItem(item);
@@ -53,10 +53,10 @@ public class StripePaymentGateway implements PaymentGateway {
 
             return switch (event.getType()) {
                 case "payment_intent.succeeded" ->
-                    Optional.of(new PaymentResult(extractOrderId(event), PaymentStatus.PAID));
+                        Optional.of(new PaymentResult(extractOrderId(event), PaymentStatus.PAID));
 
                 case "payment_intent.payment_failed" ->
-                    Optional.of(new PaymentResult(extractOrderId(event), PaymentStatus.FAILED));
+                        Optional.of(new PaymentResult(extractOrderId(event), PaymentStatus.FAILED));
 
                 default -> Optional.empty();
             };
@@ -100,6 +100,12 @@ public class StripePaymentGateway implements PaymentGateway {
     private SessionCreateParams.LineItem.PriceData.ProductData createProductData(OrderItem item) {
         return SessionCreateParams.LineItem.PriceData.ProductData.builder()
                 .setName(item.getProduct().getName())
+                .build();
+    }
+
+    private SessionCreateParams.PaymentIntentData createPaymentIntentData(Order order) {
+        return SessionCreateParams.PaymentIntentData.builder()
+                .putMetadata("order_id", order.getId().toString())
                 .build();
     }
 }
